1b5a12104cbdcbc36da54ce106e48939f5ffe0de
himanshu6388 <himanshu219181@gmail.com>
2025-03-11 19:11:14 +0530

    updated

---
+++ DsaFoundation/script.js
@@ -0,0 +1,25 @@
+
+//integer - 4 ,33,44,32 in javascript we called number
+let x = 12;
+let y =13;
+console.log(x+y)
+/*
+string + string = string(concatenation)
+string + int = string
+int + int = int(Arithmatic operations)
+*/
+// let a = "hello"
+// let b = "world"
+// console.log(typeof(a+b)) //string
+
+let c = 10;
+let d = 10;
+console.log(typeof(c+d)) // number
+
+let a = 23;
+let b = 45;
+console.log("sum of" + a + "and " + b + "is:" +( a+b));
+//"sum of 23" + "and" +b + "si" + a+b
+// "sum of  23 and "+b"is"+a+b
+
+console.log(a+b +  "is sum of "+ a + "and" +b);
