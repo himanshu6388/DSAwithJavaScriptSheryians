2e532c9a0403b51ef8e8936a18b982c8c6319a7e
himanshu6388 <himanshu219181@gmail.com>
2025-03-20 10:37:12 +0530

    updated

--- 05Loops/script.js
+++ 05Loops/script.js
@@ -1,12 +1,59 @@
-// Accept an integer and print hello world n time
+//1.  Accept an integer and print hello world n time
 
 for(i=1 ; i<=5; i++){
     //console.log(i+ ". Hello World");  // print hello world 5 times
 }
 
-//  Print natural numbers up to n
+
+
+
+
+//2.   Print natural numbers up to n
 
 let n =5;
-for  (let  i =1; i<=n ; i++){
+for  (let  i =n; i>=1; i--){
     // console.log(i)
 }
+
+
+
+
+
+//3.  Take a number as input and print its table
+
+let num = 5;
+for (let i=1; i<=10; i++){
+    let mult = num*i;
+    // console.log(num + " * " + i  +" = " + mult);
+}
+
+
+
+
+//4.  Sum up to n terms
+
+
+let term = 5;
+let sum = 0;
+for (let i=1; i<=term; i++){
+    sum += i
+}
+// console.log(sum)
+
+
+
+
+//5.  Factorial of a Number
+let x = 5;
+let fact = 1;
+for(let i=1;i<=x;i++){
+    fact *= i;
+}
+// console.log(fact);
+
+
+
+
+
+
+//6.   
