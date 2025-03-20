9d4790d1a697150b0ce9d63d14ce2799481f7954
himanshu6388 <himanshu219181@gmail.com>
2025-03-20 10:19:00 +0530

    Day 4 of dsa Loops

---
+++ 05Loops/script.js
@@ -0,0 +1,12 @@
+// Accept an integer and print hello world n time
+
+for(i=1 ; i<=5; i++){
+    //console.log(i+ ". Hello World");  // print hello world 5 times
+}
+
+//  Print natural numbers up to n
+
+let n =5;
+for  (let  i =1; i<=n ; i++){
+    // console.log(i)
+}
