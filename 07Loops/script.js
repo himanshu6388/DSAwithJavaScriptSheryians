6105091016f3b6015e4d11b89c318e94216841d9
himanshu6388 <himanshu219181@gmail.com>
2025-03-21 09:37:23 +0530

    updated

---
+++ 07Loops/script.js
@@ -0,0 +1,36 @@
+// do while 
+let i = 1;
+do{
+    // console.log("Hello world");
+    i++
+}while(i<=5);
+
+let userInput = "no";
+do{
+    // console.log("Namaste");
+    
+}while(userInput == "yes")
+
+
+
+
+
+
+
+// Guess the number
+let ran = Math.floor(Math.random()*100)
+// console.log(ran);
+// let user = ""
+// do{
+//     user = "29"
+//     if(isNaN(user) || user<0 || user >100){
+//         console.log("Please enter valid number");
+//         continue
+//     }
+//     if(user>ran) console.log("too  high, try again");
+//     else if(user < ran) console.log("too low , try again");
+//     else console.log("Congrats and Number was" + ran);
+    
+    
+// }while(user !==ran);
+
