b4c3253f779f0398cbf4d55102781bfff71f0d29
himanshu6388 <himanshu219181@gmail.com>
2025-03-22 07:21:42 +0530

    Updated

---
+++ 10Array/script.js
@@ -0,0 +1,19 @@
+// strong number
+const prompt = require('prompt-sync')();
+
+
+let  n = Number(prompt("Enter a Number:"))
+let copy = n;
+let ans =0;
+while(n>0){
+    let dig = n % 10;
+    let fact = 1;
+    for(let i =1; i<=dig;i++){
+        fact =fact*i;
+    }
+    ans = ans + fact;
+    n= Math.floor(n/10)
+}
+if(copy==ans) console.log("Strong Number");
+else console.log("Not a  Strong Number.");
+
