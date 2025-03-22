9620f748cccbf837477625c66126004e4452108c
himanshu6388 <himanshu219181@gmail.com>
2025-03-22 07:43:50 +0530

    updated

--- 10Array/script.js
+++ 10Array/script.js
@@ -2,18 +2,18 @@
 const prompt = require('prompt-sync')();
 
 
-let  n = Number(prompt("Enter a Number:"))
+let n = Number(prompt("Enter a Number:"))
 let copy = n;
-let ans =0;
+let ans = 0;
 while(n>0){
     let dig = n % 10;
     let fact = 1;
-    for(let i =1; i<=dig;i++){
-        fact =fact*i;
+    for(let i=1;i<=dig;i++){
+        fact = fact*i;
     }
     ans = ans + fact;
-    n= Math.floor(n/10)
+    n = Math.floor(n/10)
 }
-if(copy==ans) console.log("Strong Number");
-else console.log("Not a  Strong Number.");
+if(copy == ans) console.log("Strong Number");
+else console.log("Not a Strong Number.");
 
