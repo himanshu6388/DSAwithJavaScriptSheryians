7f63533c8444c46783aa094471a8bfa543ba4291
himanshu6388 <himanshu219181@gmail.com>
2025-04-04 08:33:15 +0530

    updated

--- DomProject/script.js
+++ DomProject/script.js
@@ -1,4 +1,17 @@
 // Math.random()
 
-var num =Math.floor( Math.random()*100)
-console.log(num);
+// var num =Math.floor( Math.random()*100)
+// console.log(num);
+
+
+var btn  = document.querySelector('button');
+var box = document.querySelector("#box")
+
+btn.addEventListener('click', function(){
+    var c1 = Math.floor( Math.random()*256)
+    var c2 = Math.floor( Math.random()*256)
+    var c3 = Math.floor( Math.random()*256)
+
+    box.style.backgroundColor = `rgb(${c1}, ${c2}, ${c3})`
+
+})
