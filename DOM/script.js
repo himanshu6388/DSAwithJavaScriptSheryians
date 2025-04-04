2fcd5b1d6dc4a44d926b252836281fd40df1f5ce
himanshu6388 <himanshu219181@gmail.com>
2025-04-04 06:56:42 +0530

    updaetd

--- DOM/script.js
+++ DOM/script.js
@@ -3,15 +3,25 @@
 // 1. Selection of Element  ✅
 // 2. Changing HTML         ✅
 // 3. Changing CSS          ✅
-// 4. Event Listener
+// 4. Event Listener        ✅
 
 
-var h1  = document.querySelector('h1')
+// var h1  = document.querySelector('h1')
+var h1  = document.querySelectorAll('h1')
 
+// h1[0].innerHTML = "How are You ?"
 
 h1.addEventListener('click', function(){
-    h1.innerHTML = `Hello ! How are you ?.`
+    // h1.innerHTML = `Hello ! How are you ?.`
 h1.style.color = 'white'
 h1.style.backgroundColor = 'green'
 
 })
+
+
+
+var box = document.querySelector('#box')
+box.style.backgroundColor= 'green'
+box.innerHTML = "Namaste"
+
+// querySelectorAll -> select all the element and create node list
