92d8ca302cd16bcbe1b86de7d56d5b1f17399cd6
himanshu6388 <himanshu219181@gmail.com>
2025-04-07 08:57:05 +0530

    Advance Dom concept

---
+++ AdDom/script.js
@@ -0,0 +1,10 @@
+// 1 .getAttribute and setAttribute
+
+
+let h1 = document.querySelector('h1');
+
+h1.innerHTML = 'How is it going'
+let att = h1.getAttribute('id')
+  
+h1.setAttribute('id', 'heroine')
+let img = document.querySelector('img')
