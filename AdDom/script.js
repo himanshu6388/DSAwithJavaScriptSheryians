3173cf2f33813bf0a7feb3751132843a7a8535af
himanshu6388 <himanshu219181@gmail.com>
2025-04-07 10:03:16 +0530

    updated

--- AdDom/script.js
+++ AdDom/script.js
@@ -1,15 +1,28 @@
 // 1 .getAttribute and setAttribut
 
 
-let img1 = document.querySelector("#img1")
-let  img2 = document.querySelector("#img2")
-let btn = document.querySelector("button")
+// let img1 = document.querySelector("#img1")
+// let  img2 = document.querySelector("#img2")
+// let btn = document.querySelector("button")
 
 
-btn.addEventListener('click', function(){
-    var img1Src = img1.getAttribute('src')
-    var img2Src = img2.getAttribute('src')
+// btn.addEventListener('click', function(){
+//     var img1Src = img1.getAttribute('src')
+//     var img2Src = img2.getAttribute('src')
 
-    img1.setAttribute('src', img2Src)
-    img2.setAttribute('src',  img1Src)
-})
+//     img1.setAttribute('src', img2Src)
+//     img2.setAttribute('src',  img1Src)
+// })
+
+
+
+
+
+
+
+
+// Creting an element
+let h = document.createElement('h1')
+h.innerHTML = "Hell How are you ? " 
+let body = document.querySelector('body')
+body.appendChild(h)
