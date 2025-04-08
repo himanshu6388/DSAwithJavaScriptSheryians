e0412787e9cbccaa31c7482252a1e28851364536
himanshu6388 <himanshu219181@gmail.com>
2025-04-08 08:07:31 +0530

    updated'

--- EventBubbling/script.js
+++ EventBubbling/script.js
@@ -3,21 +3,25 @@ let arr = [
     {
         user:"Himanshu",
         age:30,
-        married:false
+        married:false,
+        image:'https://static.vecteezy.com/system/resources/previews/023/842/246/non_2x/boy-cartoon-character-cute-funny-illustration-eps-10-free-vector.jpg'
     },
     {
         user:'hira',
-        age:40
+        age:40,
+        image:'https://t4.ftcdn.net/jpg/01/13/99/57/240_F_113995750_dAEGvjqxnsYD6asKjeDWJoVoSqjFvdGO.jpg'
     },
     {
         user:"salmon",
         age:65,
-        married:false
+        married:false,
+        image:'https://cdn.britannica.com/54/256354-050-C19887AD/bollywood-actor-salman-khan-at-nika-mukesh-ambani-cultural-center-nmacc-mumbai-india.jpg?w=300'
     },
     {
         user:"Shahrukh",
         age:60,
-        married:true
+        married:true,
+        image:'https://bizrahmed.com/wp-content/uploads/2024/04/Shah-Rukh-Khan3-1.webp'
     }
 ]
 
@@ -26,9 +30,20 @@ let arr = [
     
 // })
 
-var sum= 0
+// var sum= 0
 
+// arr.forEach((elem)=>{
+//    sum = sum+ elem.age
+// })
+var sum =''
 arr.forEach((elem)=>{
-   sum = sum+ elem.age
+    sum = sum + ` <div class="card">
+        <img src=${elem.image} alt="">
+        <h1>${elem.user}</h1>
+        <h5>${elem.age}</h5>
+        <h4>${elem.married}</h4>
+    </div>`
 })
-console.log(sum);
+
+
+document.body.innerHTML = sum
