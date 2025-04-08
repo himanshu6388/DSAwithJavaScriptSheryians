6549561f2881eefe521a9bf06aa9b9b59c6a7c56
himanshu6388 <himanshu219181@gmail.com>
2025-04-08 08:52:35 +0530

    Last class of Dom

---
+++ DomLast/script.js
@@ -0,0 +1,19 @@
+
+
+
+// let btn = document.querySelector('button')
+// btn.addEventListener('click',function(){
+//     location.reload()
+// })\
+
+var btn1 = document.querySelector('btn1')
+var btn2 = document.querySelector('btn2')
+
+
+btn1.addEventListener('click', function(){
+    history.back
+})
+
+btn2.addEventListener('click', function(){
+    history.forward
+})
