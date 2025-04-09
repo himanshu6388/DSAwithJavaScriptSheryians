6379602c252746b2fd37aa43f9db3bcd82f7fa70
himanshu6388 <himanshu219181@gmail.com>
2025-04-09 06:41:37 +0530

    updated

--- DomLast/script.js
+++ DomLast/script.js
@@ -6,21 +6,46 @@
 //     location.reload()
 // })\
 
-var btn1 = document.querySelector('btn1')
-var btn2 = document.querySelector('btn2')
+// var btn1 = document.querySelector('btn1')
+// var btn2 = document.querySelector('btn2')
 
 
-btn1.addEventListener('click', function(){
-    history.back
-})
+// btn1.addEventListener('click', function(){
+//     history.back
+// })
 
-btn2.addEventListener('click', function(){
-    history.forward
-})
+// btn2.addEventListener('click', function(){
+//     history.forward
+// })
 
 // localstorage.clear()
 // localstorage.setItem()
 // localstorage.getItem()
 // localstorage.removeItem()
 
-localStorage.setItem('favorite batch','live1')
+// localStorage.setItem('favorite batch','live1')
+
+var users = [
+    {
+        username:"sarthak",
+        age:25,
+        city:"bhopal"
+    },
+    {
+        username:"Himanshu",
+        age:20,
+        city:"Ayodhya"
+    },
+    {
+        username:"Sakshi",
+        age:19,
+        city:"Allahabad"
+    }
+]
+
+// var newusers = JSON.stringify(users)
+
+// localStorage.setItem('users',newusers)
+
+
+// document.cookie()
