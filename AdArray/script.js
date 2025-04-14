0e6db948ffab8c6f1fbeed3c81be7ab3a840fa6a
himanshu6388 <himanshu219181@gmail.com>
2025-04-14 07:21:45 +0530

    updated

---
+++ AdArray/script.js
@@ -0,0 +1,14 @@
+// let arr = [1,2,3,4,5];
+// let k =2;
+// k = k% arr.length
+// for(let j=1;j<=k;j++){
+//     let copy = arr[0]
+//     for(let i=0;i<arr.length-1;i++){
+//         arr[i]= arr[i+1]
+//     }
+//     arr[arr.length-1] = copy
+// }
+
+// console.log(arr);
+
+ 
