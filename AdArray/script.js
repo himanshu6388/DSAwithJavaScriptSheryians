91422cd544833343e241b17461111cebc12e4871
himanshu6388 <himanshu219181@gmail.com>
2025-04-15 07:23:23 +0530

    First Commit

--- AdArray/script.js
+++ AdArray/script.js
@@ -24,19 +24,32 @@
 
 
 
-let arr = [1,2,3,4,5]
-let k = 2;
-k = k%arr.length;
-reverse(arr, 0, k-1);
-reverse(arr, k, arr.length-1)
-reverse(arr, 0, arr.length-1)
-console.log(arr);
-function reverse(arr,i, j){
-    while(i<j){
-        let temp = arr[i]
-        arr[i] = arr[j]
-        arr[j] = temp
-        i++;
-        j--;
+// let arr = [1,2,3,4,5]
+// let k = 2;
+// k = k%arr.length;
+// reverse(arr, 0, k-1);
+// reverse(arr, k, arr.length-1)
+// reverse(arr, 0, arr.length-1)
+// console.log(arr);
+// function reverse(arr,i, j){
+//     while(i<j){
+//         let temp = arr[i]
+//         arr[i] = arr[j]
+//         arr[j] = temp
+//         i++;
+//         j--;
+//     }
+// }
+
+
+let arr = [10,5,3,15,19,69,20];
+let target = 19;
+for(let i=0;i<arr.length;i++){
+    if(arr[i]==target){
+        index = i;
+        break
     }
 }
+if(index == -1) console.log("Elelment Not found.");
+else console.log("Elelment found at " + index);
+
