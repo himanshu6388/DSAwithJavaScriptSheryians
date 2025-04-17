65fb7738ba36b03b51321a3070f7c892c784d34a
himanshu6388 <himanshu219181@gmail.com>
2025-04-17 10:05:34 +0530

    Mulit Dimension Array

--- MultiDimensionArray/script.js
+++ MultiDimensionArray/script.js
@@ -8,8 +8,24 @@ let arr = [
 
 for(let i=0;i<arr.length;i++){
     for(let j=0;j<arr[i].length;j++){
-        process.stdout.write(`${arr[i][j]}`)
+        // process.stdout.write(`${arr[i][j]}`)
     }
     console.log();
     
 }
+
+let ar = [
+    [1,2,3],
+    [4,5,6],
+    [12,8,9]
+]
+
+let leftsum = 0, rightsum = 0
+for(let i=0;i<ar.length;i++){
+    for(let j=0;j<ar[i].length;j++){
+        if(i==j) leftsum += ar[i][j];
+        if(i+j==ar.length-1) rightsum += ar[i][j]
+    }
+}
+console.log("Left sum "+ leftsum)
+console.log("Right sum "+ rightsum)
